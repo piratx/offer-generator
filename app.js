9ba6ffb25aa449a658dab8dbc710362ee9dbf6a1
konpapasteriadis <kpapasteriadis@me.com>
2026-05-26 16:26:33 +0300

    v202605261626

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
    See VERSION file for current version info
    =========================== */
 
-const VERSION = '202605261621';
+const VERSION = '202605261626';
 
 const DEFAULT_COMPANY = {
     logo:           'https://macworks.gr/macworks-logo.png',
